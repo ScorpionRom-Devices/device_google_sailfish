e3dc59ddc3a4b339e4a00ffdc17f5bba0d3af3d7
neporood <neporood@gmail.com>
2019-10-26 18:17:20 -0500

    Let's build for Sailfish
    * Gave my Marlin to my son, so I got ye ole Sailfish now

---
+++ scorpion.mk
@@ -0,0 +1,66 @@
+# Copyright (C) 2013-2018 The Dirty Unicorns Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#
+# This file is the build configuration for an aosp Android
+# build for marlin hardware. This cleanly combines a set of
+# device-specific aspects (drivers) with a device-agnostic
+# product configuration (apps). Except for a few implementation
+# details, it only fundamentally contains two inherit-product
+# lines, aosp and du, hence its name.
+#
+
+# Include common configuration
+include vendor/scorpion/config/common_full_phone.mk
+
+# Inherit from AOSP products. Most specific first.
+$(call inherit-product, device/google/marlin/aosp_sailfish.mk)
+
+# Inherit from the common Open Source product configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+
+## Camera
+PRODUCT_PACKAGES += \
+    libion
+
+## IMS
+PRODUCT_PACKAGES += \
+    com.android.ims.rcsmanager \
+    PresencePolling \
+    RcsService
+
+## TextClassifier smart selection model files
+PRODUCT_PACKAGES += \
+    textclassifier.smartselection.bundle1
+
+## Device identifier. This must come after all inclusions
+PRODUCT_NAME := scorpion_sailfish
+PRODUCT_DEVICE := sailfish
+PRODUCT_BRAND := Google
+PRODUCT_MODEL := Pixel
+TARGET_MANUFACTURER := HTC
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME="sailfish" \
+    TARGET_DEVICE="sailfish" \
+    PRIVATE_BUILD_DESC="sailfish-user 10 QP1A.191005.007.A1 5908163 release-keys"
+
+BUILD_FINGERPRINT := "google/sailfish/sailfish:10/QP1A.191005.007.A1/5908163:user/release-keys"
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.build.fingerprint=google/sailfish/sailfish:10/QP1A.191005.007.A1/5908163:user/release-keys
+
+$(call inherit-product-if-exists, vendor/google/marlin/marlin-vendor.mk)
+$(call inherit-product-if-exists, vendor/pixelgapps/pixel-gapps.mk)
